db8354db516ec11bd87a5e6e6f0d0062bba4df7d
eynullabeyli <eynullabeyliy@gmail.com>
2022-09-12 23:27:43 +0400

    logout route

--- routes/internalapi.js
+++ routes/internalapi.js
@@ -2,5 +2,8 @@ module.exports = (app) => {
     app.post('/login', (req, res) => {
         res.send("Salam")
     });
+    app.get('/logout', (req, res) => {
+        res.send("OK@200")
+    });
     return app; 
  };
